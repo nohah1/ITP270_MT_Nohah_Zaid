4c480aed8376a545f138300c0589020e9b8aa78f
Cyber Range Student <student@ip-10-1-68-32.ec2.internal>
2023-03-10 01:39:09 +0000

    Adding ping sweep results for google.com

--- MT_Nohah_Zaid.sh
+++ MT_Nohah_Zaid.sh
@@ -3,9 +3,12 @@
 read -p "Enter 'google.com' to ping, anything else is invalid: " hostname
 
 #The input validation 
-if [ "$hostname ≠ "google.com" ]; then
-	echo "Please enter  google.com. anything else is invalid"
-exit 
+if [ "$hostname" != "google.com" ]; then
+	echo "Please enter google.com. anything else is invalid"
+exit 1
 fi
  
 ping -c 5 "$hostname" > ping_results.txt
+
+#Print a message that the ping is complete
+echo "Congratulations you have successfully added results to ping_results.txt."
